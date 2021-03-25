e3d30a46891e4927d08958c9ddd3c89a2bd0aa2e
Luis montoya <luishm2024@gmail.com>
2021-03-25 18:52:04 -0500

    arreglo añadir producto , y traer los productos en la pagina shop con nuevos estilos

---
+++ Data/Model/AspNetUsers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineBlazorApp.Data.Model
+{
+    public class AspNetUsers
+    {
+        public string Id { get; set; }
+        public string UserName { get; set; }
+        public string NormalizeUserName { get; set; }
+        public string Email { get; set; }
+        public string NorlizedEmail { get; set; }
+        public string PasswordHash { get; set; }
+        public string SecurityStamp { get; set; }
+        public string ConcurrencyStamp { get; set; }
+        public string PhoneNumber { get; set; }
+        public byte PhoneNumberConfirmed { get; set; }
+        public byte TwoFactorEnabled { get; set; }
+        public string LockoutEnd { get; set; }
+        public int fk_cod_tienda { get; set; }
+
+    }
+    
+}
--- obj/Debug/netcoreapp3.1/RazorDeclaration/Pages/Shop.razor.g.cs
+++ obj/Debug/netcoreapp3.1/RazorDeclaration/Pages/Shop.razor.g.cs
@@ -13,78 +13,92 @@ namespace OnlineBlazorApp.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\User\OneDrive\Escritorio\proyecto_tienda\Tienda-Blazor\_Imports.razor"
+#line 1 "C:\Users\luish\source\repos\OnlineBlazorApp\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\User\OneDrive\Escritorio\proyecto_tienda\Tienda-Blazor\_Imports.razor"
+#line 2 "C:\Users\luish\source\repos\OnlineBlazorApp\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\User\OneDrive\Escritorio\proyecto_tienda\Tienda-Blazor\_Imports.razor"
+#line 3 "C:\Users\luish\source\repos\OnlineBlazorApp\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\User\OneDrive\Escritorio\proyecto_tienda\Tienda-Blazor\_Imports.razor"
+#line 4 "C:\Users\luish\source\repos\OnlineBlazorApp\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\User\OneDrive\Escritorio\proyecto_tienda\Tienda-Blazor\_Imports.razor"
+#line 5 "C:\Users\luish\source\repos\OnlineBlazorApp\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\User\OneDrive\Escritorio\proyecto_tienda\Tienda-Blazor\_Imports.razor"
+#line 6 "C:\Users\luish\source\repos\OnlineBlazorApp\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\User\OneDrive\Escritorio\proyecto_tienda\Tienda-Blazor\_Imports.razor"
+#line 7 "C:\Users\luish\source\repos\OnlineBlazorApp\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\User\OneDrive\Escritorio\proyecto_tienda\Tienda-Blazor\_Imports.razor"
+#line 8 "C:\Users\luish\source\repos\OnlineBlazorApp\_Imports.razor"
 using OnlineBlazorApp;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\User\OneDrive\Escritorio\proyecto_tienda\Tienda-Blazor\_Imports.razor"
+#line 9 "C:\Users\luish\source\repos\OnlineBlazorApp\_Imports.razor"
 using OnlineBlazorApp.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\User\OneDrive\Escritorio\proyecto_tienda\Tienda-Blazor\_Imports.razor"
+#line 10 "C:\Users\luish\source\repos\OnlineBlazorApp\_Imports.razor"
 using BlazorInputFile;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 11 "C:\Users\User\OneDrive\Escritorio\proyecto_tienda\Tienda-Blazor\_Imports.razor"
+#line 11 "C:\Users\luish\source\repos\OnlineBlazorApp\_Imports.razor"
 using System.IO;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
+#line 2 "C:\Users\luish\source\repos\OnlineBlazorApp\Pages\Shop.razor"
+using OnlineBlazorApp.Data.Model;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
+#line 3 "C:\Users\luish\source\repos\OnlineBlazorApp\Pages\Shop.razor"
+using OnlineBlazorApp.Data.Service;
 
 #line default
 #line hidden
@@ -97,6 +111,28 @@ using System.IO;
         {
         }
         #pragma warning restore 1998
+#nullable restore
+#line 48 "C:\Users\luish\source\repos\OnlineBlazorApp\Pages\Shop.razor"
+       
+
+    IEnumerable<Producto> productos;
+
+    protected override async Task OnInitializedAsync()
+    {
+        productos = await ProductoService.GetAllProductos();
+
+    }
+
+    void editarRegistro(int id)
+    {
+        NavigationManager.NavigateTo("/");
+    }
+
+#line default
+#line hidden
+#nullable disable
+        [global::Microsoft.AspNetCore.Components.InjectAttribute] private NavigationManager NavigationManager { get; set; }
+        [global::Microsoft.AspNetCore.Components.InjectAttribute] private IProductoService ProductoService { get; set; }
     }
 }
 #pragma warning restore 1591
